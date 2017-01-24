9d7f3d8472c8776549fc34c5f70dd1ac8c1c90a4
Markus Josef Romstorfer <a1200478@unet.univie.ac.at>
2017-01-24 14:14:51 +0100

    Startseite Analytiker fertiggestellt

--- MyEvents/src/servlets/AnalytikerServlet.java
+++ MyEvents/src/servlets/AnalytikerServlet.java
@@ -48,8 +48,12 @@ public class AnalytikerServlet extends HttpServlet {
 				+ "        </div>"
 				+ "        <div id=\"navbar\" class=\"navbar-collapse collapse\">"
 				+ "          <ul class=\"nav navbar-nav\">"
-				+ "            <li class=\"active\"><a href=\"#\">Startseite</a></li>"
-				+ "          </ul>"
+				+ "            <li class=\"active\"><a href=\"#\">Startseite</a></li>");
+    		for(java.util.Iterator<java.util.Map.Entry<String, String> > it = AnalytikerManagement.getReportList().entrySet().iterator(); it.hasNext(); ) {
+				java.util.Map.Entry<String, String> e = it.next();
+				response.getWriter().println("<li><a href=\"?step=1&report="+ e.getKey() + "\">"+ e.getValue() + "</a></li>");
+			}
+    		response.getWriter().println("</ul>"
 				+ "          <ul class=\"nav navbar-nav navbar-right\">"
 				+ "            <li><a href=\"javascript:{}\" onclick=\"document.getElementById('my_form').submit(); return false;\">Logout</a></li>"
 				+ "          </ul>"
@@ -62,15 +66,8 @@ public class AnalytikerServlet extends HttpServlet {
 				+ "      <div class=\"jumbotron\">"
 				+ "        <h1>Willkommen!</h1>"
 				+ "        <p>Willkommen auf der Hauptseite des Analytikers</p>"
+				+ "        <p>Bitte wähle oben eine Statisik!</p>"
 				+ "      </div>"
-				+ "<h1 class=\"display-4\">List of Reporting</h1><ul class=\"list-group\">");
-
-			for(java.util.Iterator<java.util.Map.Entry<String, String> > it = AnalytikerManagement.getReportList().entrySet().iterator(); it.hasNext(); ) {
-				java.util.Map.Entry<String, String> e = it.next();
-				response.getWriter().println("<li class=\"list-group-item\"><a href=\"?step=1&report="+ e.getKey() + "\">"+ e.getValue() + "</a></li>");
-			}
-				
-			response.getWriter().println("</ul>"
 				+ "    </div>"
 				+ "<!-- Bootstrap core JavaScript"
 				+ "    ================================================== -->"
